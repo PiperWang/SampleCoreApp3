f1a71fb796784cce9dde389832e7a532f115fbcc
luoli911 <v-liiluo@microsoft.com>
2017-11-24 06:28:38 +0000

    ASP.NET Core for multi-containers

---
+++ SampleCoreApp3/Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleCoreApp3.Models;
+
+namespace SampleCoreApp3.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+            if(context.Products.Any())
+            {
+                return;
+            }
+            var products = new List<Product>
+            {
+                new Product{Name="NoteBook",Price=5},
+                new Product{Name="CellPhone",Price=1800},
+                new Product{Name="CD Player",Price=30},
+                new Product{Name="Jewelry",Price=800}
+            };
+            products.ForEach(s => context.Products.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ SampleCoreApp3/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SampleCoreApp3.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleCoreApp3
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=CoreDemoDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+            // This line uses 'UseSqlServer' in the 'options' parameter
+            // with the connection string defined above.
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(connection));
+
+
+            services.AddMvc();
+
+            // Add application services.
+            //services.AddTransient<IEmailSender, AuthMessageSender>();
+            //services.AddTransient<ISmsSender, AuthMessageSender>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            app.UseStaticFiles();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+            app.UseMvc(routes => {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
